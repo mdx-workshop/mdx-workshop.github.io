8a0eb0d311820a25b17fcd05368a234281dfccce
Fabian-Robert Stöter <mail@faroit.com>
2021-05-29 16:18:17 +0200

    add nice graphics

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -4,24 +4,28 @@ import Divider from "../elements/divider"
 import Inner from "../elements/inner"
 import Content from "../elements/content"
 import SVG from "./svg"
+import IMG from "./img"
 import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 import Intro from "../sections/intro"
+import sax from "../elements/sax.png"
+import piano from "../elements/piano.png"
+import drums from "../elements/drums.png"
+import headphones from "../elements/headphones.png"
 
 const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
   <div>
     <Divider speed={0.2} offset={offset} factor={factor}>
       <UpDown>
-        <SVG icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
-        <SVG icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
+        <IMG href={sax} width={100} top="20%" left="10%" />
+        <IMG href={piano} width={100} top="60%" left="50%"/>
         <SVG icon="box" width={6} color="icon_darker" left="60%" top="15%" />
       </UpDown>
       <UpDownWide>
-        <SVG icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
-        <SVG icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+        <IMG href={headphones} width={140} top="50%" left="88%" />
         <SVG icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
         <SVG icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
-        <SVG icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
+        <IMG href={drums} width={100} top="15%" left="75%" />
         <SVG icon="circle" width={6} color="icon_darkest" left="75%" top="10%" />
         <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
       </UpDownWide>
