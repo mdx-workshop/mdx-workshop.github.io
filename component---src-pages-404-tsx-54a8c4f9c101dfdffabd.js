b98e674fca0d0e891b5da16eddb24a0ed9d766f8
Fabian-Robert Stöter <mail@faroit.com>
2023-09-07 12:46:42 +0200

    Updates

---
+++ component---src-pages-404-tsx-54a8c4f9c101dfdffabd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[218],{2513:function(e,l,n){n.r(l);var t=n(7294),u=n(6920);l.default=e=>t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-54a8c4f9c101dfdffabd.js.map
